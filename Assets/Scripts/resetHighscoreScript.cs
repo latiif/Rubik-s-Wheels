9a48710fd17153036059018ba0f931221d19a124
oswalchlm <waoscar@student.chalmers.se>
2024-01-12 21:42:56 +0100

    added reset confirm text

--- Assets/Scripts/resetHighscoreScript.cs
+++ Assets/Scripts/resetHighscoreScript.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 using UnityEngine.UI;
 public class resetHighscoreScript : MonoBehaviour
 {
     Button resetButton;
+    [SerializeField] private TMP_Text _resetText;
+
     // Start is called before the first frame update
     void Start()
     {
+        _resetText.enabled = false;
         resetButton = GetComponent<Button>();
         resetButton.onClick.AddListener(delegate
         {
             PlayerPrefs.SetInt("high-score", 0);
+            _resetText.text = "Highscore reset to 0";
+            _resetText.enabled = true;
         });
     }
 
